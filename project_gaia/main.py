58653e13941c096823ea89dc2a81ce7787d3eba3
Anthony Okafor <legendawakening2000@gmail.com>
2025-06-25 09:16:34 +0100

    Switched project to Flet app

---
+++ project_gaia/main.py
@@ -0,0 +1,8 @@
+import flet as ft
+import os
+
+def main(page: ft.Page):
+    page.title = "Business AI App"
+    page.add(ft.Text("Hello from Flet!"))
+
+ft.app(target=main, port=int(os.environ.get("PORT", 8000)), view=ft.AppView.FLET_APP_WEB, assets_dir="assets")
