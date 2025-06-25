05c9133da74d4f03d297e4243af9bf8525b23463
Anthony Okafor <legendawakening2000@gmail.com>
2025-06-25 01:39:40 +0100

    Connected core app to main URL routing

--- project_gaia/urls.py
+++ project_gaia/urls.py
@@ -4,3 +4,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),  # 👈 this connects your app
+]
