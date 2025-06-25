a030e09fcfcfe690b0bf2a6b7ea7b5fce463d0f2
Anthony Okafor <legendawakening2000@gmail.com>
2025-06-25 04:11:19 +0100

    Added static files config

--- project_gaia/project_gaia/settings.py
+++ project_gaia/project_gaia/settings.py
@@ -123,3 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "project_gaia" / "core" / "static"]
